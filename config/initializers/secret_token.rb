c73bbb9a37d5a1210c41d2a8c9d5b57dd8c43d52
Jacqueline Chenault <adigitalnative@gmail.com>
2015-01-25 16:08:13 -0500

    Initial rails isntall with RSpec & Capybar

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Giant::Application.config.secret_token = '6620a3038e7bcc28b3e5eca0eb28af10f7b7acd760a87c4309dd672a7aa6d5303b194832a1fcde4a0c40b27d3b15af518130f02a6a53a1d453d354f392c93707'
